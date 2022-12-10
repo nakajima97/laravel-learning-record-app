689a44dc258bee82ce0af381974e60f7a4e4045d
Yudai Nakajima <kituneudon97@gmail.com>
2022-12-10 09:27:10 +0000

    #19 勉強記録画面の雛形作成

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RecordController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('records' , RecordController::class);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
