78e903117f32b17ac21b7a8edd357355af0e46de
Yudai Nakajima <kituneudon97@gmail.com>
2022-12-13 12:59:52 +0000

    #34 メインカテゴリーを表示するようにした

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -15,7 +15,9 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        return view('categories.index');
+        $categories = MainCategory::where('user_id', Auth::id())->get();
+
+        return view('categories.index', compact('categories'));
     }
 
     /**
--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -9,9 +9,13 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    カテゴリー一覧画面
                     <div>
                         <a href="{{ route('categories.create') }}" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">カテゴリー追加</a>
+                    </div>
+                    <div class="my-4">
+                        @foreach ($categories as $category)
+                            <h2 class="text-4xl font-extrabold dark:text-white">{{ $category->name }}</h2>
+                        @endforeach
                     </div>
                 </div>
             </div>
