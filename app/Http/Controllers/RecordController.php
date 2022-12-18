0c80f08a78a8e2f258696e5e87c900ad1c0ffba2
Yudai Nakajima <kituneudon97@gmail.com>
2022-12-18 17:11:52 +0900

    #20 勉強記録を保存できるようにした

--- app/Http/Controllers/RecordController.php
+++ app/Http/Controllers/RecordController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\MainCategory;
+use App\Models\StudyRecord;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -43,7 +44,11 @@ class RecordController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $study_record = new StudyRecord();
+        $study_record->fill($request->all());
+        $study_record->save();
+
+        return redirect()->route('records.index');
     }
 
     /**
--- resources/views/records/create.blade.php
+++ resources/views/records/create.blade.php
@@ -9,16 +9,17 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <form>
+                    <form action="{{ route('records.store') }}" method="POST">
+                        @csrf
                         <div class="mb-6">
                             <label for="email"
                                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">サブカテゴリー</label>
-                            <x-myComponents.select id="sub_categeory" name="sub_category" :options="$sub_categories" />
+                            <x-myComponents.select id="sub_categeory" name="sub_category_id" :options="$sub_categories" />
                         </div>
                         <div class="mb-6">
                             <label for="password"
                                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">勉強時間</label>
-                            <x-myComponents.input type="text" id="time" name="time" />
+                            <x-myComponents.input type="text" id="minute" name="minute" />
                         </div>
                         <button type="submit"
                             class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">登録</button>
